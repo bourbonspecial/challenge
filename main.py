defea7e98d29ae5f54a7d1f596c22418073e9b56
Remus Knowles <remknowles@gmail.com>
2017-09-13 11:42:11 +0100

    check for correlations between input variables

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ __author__ = 'Remus Knowles <remknowles@gmail.com>'
 import pandas as pd
 
 F_DATA = r'data challenge test.csv'
+F_CORRELATIONS = r'correlations.csv'
 
 def main():
 	df = pd.read_csv(F_DATA)
+	df.drop('id', axis=1, inplace=True)
 
-	print df.head()
+	# Split off the data we want to put in to our model.
+	df = df[:60]
+	df_to_predict = df[60:]
+
+	# Check for any obvious correlation in explanatory variables.
+	print df.corr().to_csv(F_CORRELATIONS)
 
 if __name__ == '__main__':
 	main()
