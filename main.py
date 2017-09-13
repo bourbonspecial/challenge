bf98eaddcdf1946d8aae6850d58e3ea6a1c09964
Remus Knowles <remknowles@gmail.com>
2017-09-13 12:57:34 +0100

    Don't forget to scale all variables first!

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def main():
 	df = df[:60]
 	df_to_predict = df[60:]
 
-	x = df['groups']
-	y = df.drop('groups', axis=1)
+	y = df['groups']
+	x = df.drop('groups', axis=1)
 
 	# Most data is already pretty well behaved but no harm in scaling.
 	preprocessing.scale(x)
@@ -28,9 +28,7 @@ def main():
 	y_train = y[:50]
 	y_test = y[50:]
 
-	print len(x_train), len(x_test)
-	print x_train.head()
-	print y_train.head()
+	
 
 if __name__ == '__main__':
 	main()
